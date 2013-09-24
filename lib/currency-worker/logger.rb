25bd12a9efc49fc108ac6216d86b7e6d60aef122
Jaco le Roux <jleroux@amazon.com>
2013-09-25 00:52:04 +0200

    Added timestamps to logger

--- lib/currency-worker/logger.rb
+++ lib/currency-worker/logger.rb
@@ -8,6 +8,7 @@ module CurrencyWorker
 
     def self.setup
       log = Log4r::Logger.new(self.name.split("::").first)
+      Log4r::Outputter.stdout.formatter = Log4r::PatternFormatter.new({:pattern => '%d %l %c - %m'})
       log.outputters << Log4r::Outputter.stdout
     end
   end
